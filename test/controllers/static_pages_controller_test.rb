19067b3c782f2e22caf3af4acd3546e3284dd347
christian <cbachert@users.noreply.github.com>
2017-03-04 16:46:01 +0000

    Added checks to test if mandatory legal information has been supplied via environment variables, corrected paths to check

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,30 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get impressum" do
-    get static_pages_impressum_url
+    get impressum_path
     assert_response :success
   end
 
+  test "should check variables used in impressum" do
+    assert_not_nil(ENV["IMPRESSUM_NAME"])
+    assert_not_empty(ENV["IMPRESSUM_NAME"])
+
+    assert_not_nil(ENV["IMPRESSUM_ADDRESS"])
+    assert_not_empty(ENV["IMPRESSUM_ADDRESS"])
+
+    assert_not_nil(ENV["IMPRESSUM_PHONE"])
+    assert_not_empty(ENV["IMPRESSUM_PHONE"])
+
+    # Fax is optional
+    # assert_not_nil(ENV["IMPRESSUM_FAX"])
+    # assert_not_empty(ENV["IMPRESSUM_FAX"])
+
+    assert_not_nil(ENV["IMPRESSUM_EMAIL"])
+    assert_not_empty(ENV["IMPRESSUM_EMAIL"])
+  end
+
   test "should get datenschutz" do
-    get static_pages_datenschutz_url
+    get datenschutz_path
     assert_response :success
   end
 
