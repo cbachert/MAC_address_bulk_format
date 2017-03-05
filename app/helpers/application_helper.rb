09aae2023c888fd385819dd624503242e3e89b81
christian <cbachert@users.noreply.github.com>
2017-03-05 02:31:37 +0000

    Add check for nil and empty helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nil_or_empty(variable)
+    if variable.nil?
+      return true
+    elsif variable.empty?
+      return true
+    else
+      return false
+    end
+  end
 end
